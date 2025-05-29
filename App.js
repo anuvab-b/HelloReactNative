f4d9675b0e836a5bf39b150a62c5a69c93738eab
anuvab-b <anuvabbiswas12@gmail.com>
2025-05-29 23:29:18 +0530

    Header & Footer

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import { View, Text } from "react-native";
-
+import { View } from "react-native";
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo Hello World</Text>
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#495E57",
+        }}
+      >
+        <LittleLemonHeader />
+      </View>
+      <View style={{ backgroundColor: "#495E57" }}>
+        <LittleLemonFooter />
+      </View>
+    </>
   );
 }

950cad138e1dbff27d69f1fe89fe9fd6a8b97c05
anuvab-b <anuvabbiswas12@gmail.com>
2025-05-31 17:00:40 +0530

    Tab Navigation

--- App.js
+++ App.js
@@ -6,18 +6,34 @@ import SectionMenuItems from "./components/SectionMenuItems";
 import LoginScreen from "./components/LoginScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <>
       <NavigationContainer>
         <View style={styles.container}>
           <LittleLemonHeader />
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ size }) => {
+                let iconName;
+                if (route.name === "Welcome") {
+                  iconName = "home";
+                } else if (route.name === "Login") {
+                  iconName = "arrow-forward";
+                }
+                return <Ionicons name={iconName} size={size} />;
+              },
+            })}
+            initialRouteName="Login"
+          >
+            <Tab.Screen name="Welcome" component={WelcomeScreen}></Tab.Screen>
+            <Tab.Screen name="Login" component={LoginScreen}></Tab.Screen>
+          </Tab.Navigator>
         </View>
         <View style={styles.footerContainer}>
           <LittleLemonFooter />
